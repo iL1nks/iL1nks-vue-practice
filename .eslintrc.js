3a443c43faa17744c074ad86a570b612da81a443
iLinks <3043564076@qq.com>
2022-08-19 22:32:48 +0800

    environment

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:vue/essential"
+//     ],
+//     "parserOptions": {
+//         "ecmaVersion": 12,
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "vue"
+//     ],
+//     "rules": {
+//     }
+// };
+
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    'extends': [
+      'plugin:vue/essential',
+      'eslint:recommended'
+    ],
+    parserOptions: {
+      parser: '@babel/eslint-parser'
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
+  
